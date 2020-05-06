c66eb4113f9e129b1f13bcba7766928c61451afe
Jared Parnell <jared.parnell@opendataservices.coop>
2020-05-06 16:50:52 +0100

    flags: position flags on top right, and ensure persons get nationality presented

--- src/nodes/nodeHTMLLabel.js
+++ src/nodes/nodeHTMLLabel.js
@@ -1,19 +1,19 @@
-const generateNodeLabel = (nodeType, nodeText, jurisdiction) => {
-  const countryCode = jurisdiction ? jurisdiction.code : null;
+const generateNodeLabel = (nodeType, nodeText, countryCode) => {
   return `
     <div class='centred-label'>
-        <div class="image-holder">
-            <div class="node-glyph top-left">
-                ${countryCode ? `<img class="node-flag" src="public/assets/flags/${countryCode}.svg"/>` : ''}
-            </div>
-            <div class="node-glyph top-right"></div>
-            <div class="node-glyph bottom-left"></div>
-            <div class="node-glyph bottom-right"></div>
-            <img class="node-image" src="public/assets/${nodeType}.svg"></img><br>
+      <div class="image-holder">
+      <div class="node-glyph top-left">
+      </div>
+      <div class="node-glyph top-right">
+        ${countryCode ? `<img class="node-flag" src="public/assets/flags/${countryCode}.svg"/>` : ''}
         </div>
-        <div class="node-label wrap-text">
-            ${nodeText}
-        </div>
+        <div class="node-glyph bottom-left"></div>
+        <div class="node-glyph bottom-right"></div>
+        <img class="node-image" src="public/assets/${nodeType}.svg"></img><br>
+      </div>
+      <div class="node-label wrap-text">
+        ${nodeText}
+      </div>
     </div>
     `;
 };
--- src/nodes/nodes.js
+++ src/nodes/nodes.js
@@ -19,11 +19,12 @@ export const getPersonNodes = (bodsData) => {
   return bodsData
     .filter((statement) => statement.statementType === 'personStatement')
     .map((statement) => {
-      const { statementID, names, incorporatedInJurisdiction = null } = statement;
+      const { statementID, names, nationalities = null } = statement;
       const nodeType = statementID === 'unknown' ? 'unknown' : 'person';
+      const countryCode = nationalities ? nationalities[0].code : null
       return {
         id: statementID,
-        label: generateNodeLabel(nodeType, names[0].fullName, incorporatedInJurisdiction),
+        label: generateNodeLabel(nodeType, names[0].fullName, countryCode),
         labelType: 'html',
         class: nodeType,
         config: nodeConfig,
@@ -36,9 +37,10 @@ export const getEntityNodes = (bodsData) => {
     .filter((statement) => statement.statementType === 'entityStatement')
     .map((statement) => {
       const { statementID, name, incorporatedInJurisdiction = null } = statement;
+      const countryCode = incorporatedInJurisdiction ? incorporatedInJurisdiction.code : null;
       return {
         id: statementID,
-        label: generateNodeLabel('entity', name, incorporatedInJurisdiction),
+        label: generateNodeLabel('entity', name, countryCode),
         labelType: 'html',
         class: 'entity',
         config: nodeConfig,
