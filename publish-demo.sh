f02c5245edc9c684c940c8e7df364340936c0747
Steve Day <steve@openownership.org>
2020-09-09 10:36:53 +0100

    Skip CI on gh-pages builds

--- publish-demo.sh
+++ publish-demo.sh
@@ -3,7 +3,7 @@ git worktree add demo-build gh-pages
 npm run demo
 cd demo-build
 git add --all
-git commit -m "New demo build"
+git commit -m "New demo build [ci skip]"
 git push origin gh-pages
 cd ..
 git worktree remove demo-build
