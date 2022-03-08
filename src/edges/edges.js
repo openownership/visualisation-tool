888f227dc947ce05ccde81a1829102c6a7b85365
Jared Parnell <jared.parnell@opendataservices.coop>
2022-03-08 17:14:54 +0000

    indirect: Add indirect relationship formatting
    https://github.com/openownership/visualisation-tool/issues/35

--- src/edges/edges.js
+++ src/edges/edges.js
@@ -36,9 +36,17 @@ export const getOwnershipEdges = (bodsData) => {
       .map((statement) => {
         const { statementID, subject, interestedParty, interests } = statement;
         const replaces = statement.replacesStatements ? statement.replacesStatements : [];
+        const { interestLevel, directOrIndirect } = interests[0] || { interestLevel: 'unknown' };
+        // this accounts for changes from 0.2 to 0.3 (interestLevel renamed to directOrIndirect)
+        const interestRelationship = interestLevel
+          ? interestLevel
+          : directOrIndirect
+          ? directOrIndirect
+          : 'direct';
         return {
           id: statementID,
           interests: getInterests(interests),
+          interestRelationship,
           source:
             interestedParty.describedByPersonStatement ||
             interestedParty.describedByEntityStatement ||
