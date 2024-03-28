180e73f576e4779cf17f979e558dcf2b1bb10711
Myles Lewando <myles.lewando@opendataservices.coop>
2024-03-28 16:09:26 +0000

    Remove further sanitisation

--- src/nodes/nodeSVGLabel.js
+++ src/nodes/nodeSVGLabel.js
@@ -1,14 +1,11 @@
-import sanitise from '../utils/sanitiser';
-
 const generateNodeLabel = (nodeText) => {
-  const sanitisedNodeText = sanitise(nodeText);
   const text_label = document.createElementNS('http://www.w3.org/2000/svg', 'g');
   const svg_label = document.createElementNS('http://www.w3.org/2000/svg', 'text');
   svg_label.setAttribute('x', 0);
   svg_label.setAttribute('y', 90);
   const tspan = document.createElementNS('http://www.w3.org/2000/svg', 'tspan');
   tspan.setAttributeNS('http://www.w3.org/XML/1998/namespace', 'xml:space', 'preserve');
-  tspan.textContent = sanitisedNodeText;
+  tspan.textContent = nodeText;
   svg_label.appendChild(tspan);
   text_label.appendChild(svg_label);
 
