1266bf411b2a712a76fb9442324ff39251dffa72
Myles Lewando <myles.lewando@opendataservices.coop>
2024-09-30 18:54:03 +0100

    Switch to vitest

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('jest').Config} */
-export default {
-  collectCoverage: false,
-  collectCoverageFrom: ['**/src/**'],
-  verbose: true,
-};
--- tests/html.test.js
+++ tests/html.test.js
@@ -1,7 +1,3 @@
-/**
- * @jest-environment jsdom
- */
-
 import fs from 'fs';
 import path from 'path';
 
