d928a4b9e78d5f50ff3a84d32020adc9e3acd316
Myles Lewando <myles.lewando@opendataservices.coop>
2024-11-19 11:38:18 +0000

    Update selected date when apply button pressed

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,4 @@
-import { draw } from '../src/index.js';
+import { draw, selectData } from '../src/index.js';
 import { clearSVG } from '../src/utils/svgTools.js';
 import { parse } from '../src/parse/parse.js';
 import './demo.css';
@@ -38,9 +38,11 @@ const getJSON = async () => {
 const visualiseData = (data) => {
   // Render data as text
   document.getElementById('result').value = data.formatted;
+  // Select data to render
+  const selectedData = selectData(data.parsed);
   // Render data as graph
   draw({
-    data: data.parsed,
+    data: selectedData,
     container: document.getElementById('svg-holder'),
     imagesPath: 'images',
     labelLimit: 100,
