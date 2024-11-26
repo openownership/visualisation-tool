7f739626b886a5d6e85e134a291c31bed0e38ea7
Myles Lewando <myles.lewando@opendataservices.coop>
2024-11-26 13:20:02 +0000

    Remove unused 'latest' function

--- src/utils/bods.js
+++ src/utils/bods.js
@@ -122,37 +122,3 @@ export const filteredData = (statements, selectedDate, version) => {
     });
   }
 };
-
-export const latest = (statements, closedRecords, version) => {
-  if (compareVersions(version, '0.4') >= 0) {
-    const statementMap = {};
-
-    statements.forEach((statement) => {
-      const { recordId, recordStatus, source } = statement;
-
-      if (recordStatus === 'closed') {
-        closedRecords.add(recordId);
-        closedRecords.add(source);
-      }
-    });
-
-    statements.forEach((statement) => {
-      const { recordId, statementDate, recordStatus } = statement;
-
-      if (recordStatus === 'closed' || closedRecords.has(recordId)) {
-        return;
-      }
-
-      if (
-        !statementMap[recordId] ||
-        new Date(statementDate) > new Date(statementMap[recordId].statementDate)
-      ) {
-        statementMap[recordId] = statement;
-      }
-    });
-    return Object.values(statementMap);
-  } else {
-    const replaced = new Set(statements.flatMap((s) => s.replaces));
-    return statements.filter((s) => !replaced.has(s.id));
-  }
-};
